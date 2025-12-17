057f3abb88ee17432681c2c286b4e7c312723074
thelaycon <tobitobitobiwhy@gmail.com>
2025-12-17 10:32:55 +0100

    Update div_signed.asm

--- chapter_2/div_signed.asm
+++ chapter_2/div_signed.asm
@@ -2,11 +2,7 @@ section .text
 	global _start
 
 _start:
-	; There are ideally two ways to divide in assembly.
-	; First is to use the unsigned division instruction, which is called div.
-	; Second is to use the signed division instruction, which is called idiv.
-
-
+	; Secondly, we use the signed division instruction, which is called idiv.
 	mov rax, -10;
 	mov rbx, 3;
 
